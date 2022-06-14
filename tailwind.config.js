c1b574667e69c8611d9dbf7422fd4fa01b475340
Tanvir Shakil <tanvirshakil777@gmail.com>
2022-06-14 10:12:10 +0600

    chaning line xs -1

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
   theme: {
     extend: {
       screens: {
-        xs: "380px",
+        xs: {'max': '400px'},
         sm: "640px",
         md: "768px",
         lg: "1024px",
